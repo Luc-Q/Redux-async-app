a237524f2c3b127fe7919c1f5f91da8b55b2e488
Lu <jetqin_2008@hotmail.com>
2022-02-23 14:32:32 +1100

    Redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODDUCTS = [
+  { id: 'p1', price: 6, title: 'my first book', description: 'the first book'},
+  { id: 'p2', price: 5, title: 'my second book', description: 'the second book'}
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+        {DUMMY_PRODDUCTS.map((product) => (
+          <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
         />
+        ) )}
       </ul>
     </section>
   );
